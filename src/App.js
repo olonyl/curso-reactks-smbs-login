49199f3ecd595d371bed17fb4d3a791b8bf15641
Olonyl Horacio Rocha Landeros <jose87.landeros@gmail.com>
2018-12-22 11:01:19 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,71 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      error: ''
+    }
+
+  }
+
+  handleChange = (event) => {
+    var name = event.target;
+    var value = event.value;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!this.state.username) {
+      this.setState({
+        error: 'Username is required'
+      });
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <div className="Login">
+            <div className="card">
+              <div className="card-header">
+                Login
+              </div>
+              <div className="card-body">
+                <form action="" onSubmit={this.handleSubmit}>
+                  <div className="row">
+                    <div className="col-12 AvatarWrapper">
+                      <img src="/avatar.png" alt="" className="Avatar" />
+                    </div>
+                    <div className="col-12">
+                      <div className="alert alert-danger mt-2">
+                        Error: <br /> {this.state.error}
+                      </div>
+                    </div>
+                    <div className="col-12">
+                      <label htmlFor="">Username</label>
+                      <input type="text" name="username" className="form-control" onChange={this.handleChange} />
+                    </div>
+                    <div className="col-12">
+                      <label htmlFor="">Password</label>
+                      <input type="password" name="username" className="form-control" onChange={this.handleChange} />
+                    </div>
+                    <div className="col-12 mt-2">
+                      <input type="submit" name="Login" className="btn btn-success" value="Login" />
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
